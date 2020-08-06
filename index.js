0240aebf9115e9e7c0f0360a1d7843528819b500
socabo <socabo@users.noreply.github.com>
2020-08-06 08:19:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 function add (a, b) {
   return a + b
 }
-// some change
-
-function sub (a, b) {
-  return a - b
-}
-
-function sub (a, b) {
-  return a - b
-}
-
-function subr (a, b) {
-  return a - b
-}
 
 // function subr (a, b) {
 //   return a - b
